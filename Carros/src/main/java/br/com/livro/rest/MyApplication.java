d4047556af5b224461f711ab971bc0cf5d233b5b
Douglas Prado <dc.prado@gmail.com>
2019-09-13 00:05:52 -0300

    Upload files

--- Carros/src/main/java/br/com/livro/rest/MyApplication.java
+++ Carros/src/main/java/br/com/livro/rest/MyApplication.java
@@ -1,18 +1,22 @@
 package br.com.livro.rest;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import javax.ws.rs.core.Application;
 
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+
 public class MyApplication extends Application {
 
-//	public Set<Object> getSingletons() {
-//		Set<Object> singletons = new HashSet<>();
-//		// Driver do Jettison para gerar o JSON.
-//		singletons.add(new JettisonFeature());
-//		return singletons;
-//	}
+	public Set<Object> getSingletons() {
+		Set<Object> singletons = new HashSet<>();
+		// Driver do Jettison para gerar o JSON.
+		singletons.add(new MultiPartFeature());
+		return singletons;
+	}
 
 	public Map<String, Object> getProperties() {
 		Map<String, Object> properties = new HashMap<String, Object>();
